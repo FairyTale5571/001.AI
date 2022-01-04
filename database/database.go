68b3262db513b9b7dfe2a6e3aceacfb9e34b8915
Aleksandr <a3atlaslife@gmail.com>
2022-01-04 21:15:26 +0200

    connect gorm

--- database/database.go
+++ database/database.go
@@ -1,11 +1,52 @@
 package database
 
-import "database/sql"
+import (
+	"001.AI/logger"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
 
-var db *sql.DB
+var db *gorm.DB
 
-func ConnectDatabase() (*sql.DB, error) {
+type RulesChannels struct {
+	gorm.Model
+	Id uint32
+	GuildId string
+	ChannelId string
+}
 
+type ConLeaveChannels struct {
+	gorm.Model
+
+	Id uint32
+	GuildId string
+	ChannelId string
+}
+
+type ConnectLogs struct {
+	gorm.Model
+
+	Id uint32
+	GuildId string
+	UserId string
+	UserName string
+	UserDiscriminator string
+}
+
+func ConnectDatabase() (*gorm.DB, error) {
+
+	db, err := gorm.Open(sqlite.Open("001_database.db"), & gorm.Config{})
+	if err != nil {
+		logger.PrintLog("cant open database %s\n", err.Error())
+		return db, err
+	}
+	db.AutoMigrate(RulesChannels{})
+	db.AutoMigrate(ConLeaveChannels{})
+	db.AutoMigrate(ConnectLogs{})
+
+	db.Create(&RulesChannels{})
+	db.Create(&RulesChannels{})
+	db.Create(&ConnectLogs{})
 
 	return db, nil
 }
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"001.AI/bot"
 	"001.AI/config"
+	"001.AI/database"
 	"fmt"
 	"log"
 )
@@ -14,9 +15,16 @@ func main() {
 		fmt.Println(err.Error())
 		return
 	}
+
 	fmt.Printf("\n   ____  ____ ___  ___    ____\n  / __ \\/ __ <  / /   |  /  _/\n / / / / / / / / / /| |  / /  \n/ /_/ / /_/ / / / ___ |_/ /   \n\\____/\\____/_(_)_/  |_/___/   \n                              \n")
 	log.Printf("001k AI will be started in few seconds\n")
 	log.Printf("v%s\n",config.GetVersion())
+
+	_, err = database.ConnectDatabase()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
 	bot.Start()
 	return
 }
