2858eea2544531a60138be7e423e65d1fcdd9d20
FairyTale <dev@rimasrp.life>
2022-01-12 23:00:35 +0200

    give remove roles

--- bot/roles.go
+++ bot/roles.go
@@ -3,26 +3,124 @@ package bot
 import (
 	"001.AI/database"
 	"001.AI/logger"
+	"fmt"
 	"github.com/bwmarrin/discordgo"
 )
 
 func isAdmin(user *discordgo.User, channel string) bool {
 	_, err := s.UserChannelPermissions(user.ID, channel)
 	if err != nil {
-		logger.PrintLog("cant check permission %s\n",err.Error())
+		logger.PrintLog("cant check permission %s\n", err.Error())
 		return false
 	}
 	return false
 }
 
+func giveVerifiedRoles(guildId string, user *discordgo.User) {
+	fmt.Printf("%s\n", user)
+	roles, err := database.GetVerifiedRoles(guildId)
+	if err != nil {
+		logger.PrintLog("get rule channel error: %s\n", err.Error())
+	}
+	if len(roles) == 0 {
+		logger.PrintLog("connect messages not configured\n")
+		return
+	}
+	for _, role := range roles {
+		fmt.Printf("role: %s|%s|%s\n", guildId, user.ID, role)
+		giveRole(guildId, user.ID, role)
+	}
+}
+
+func removeVerifiedRoles(guildId string, user *discordgo.User) {
+	roles, err := database.GetVerifiedRoles(guildId)
+	if err != nil {
+		logger.PrintLog("get rule channel error: %s\n", err.Error())
+	}
+	if len(roles) == 0 {
+		logger.PrintLog("connect messages not configured\n")
+		return
+	}
+	for _, role := range roles {
+		removeRole(guildId, user.ID, role)
+	}
+}
+
+func giveRole(guildId string, uid string, roleId string) {
+	fmt.Printf("giving role \n")
+	_, err := s.GuildMember(guildId, uid)
+	if err != nil {
+		logger.PrintLog("error: give role - user undefined\n")
+		return
+	}
+	err = s.GuildMemberRoleAdd(guildId, uid, roleId)
+	if err != nil {
+		logger.PrintLog("error: give role - %s\n", err.Error())
+		return
+	}
+}
+
+func removeRole(guildId string, uid string, roleId string) {
+	_, err := s.GuildMember(guildId, uid)
+	if err != nil {
+		logger.PrintLog("error: remove role - user undefined\n")
+		return
+	}
+	err = s.GuildMemberRoleRemove(guildId, uid, roleId)
+	if err != nil {
+		logger.PrintLog("error: remove role - %s\n", err.Error())
+		return
+	}
+}
+
+// сохранить роль
 func setVerifiedRole(s *discordgo.Session, i *discordgo.InteractionCreate) {
-	role := i.ApplicationCommandData().Options[0].RoleValue(s,"")
-	database.SetVerifiedRole(i.GuildID,role.ID)
+	role := i.ApplicationCommandData().Options[0].RoleValue(s, "")
+	database.SetVerifiedRole(i.GuildID, role.ID)
 	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 		Type: discordgo.InteractionResponseChannelMessageWithSource,
 		Data: &discordgo.InteractionResponseData{
-			Content: "Роль "+ role.Mention() +" установлена",
-			Flags: 1 << 6,
+			Content: "Роль " + role.Mention() + " установлена",
+			Flags:   1 << 6,
+		},
+	})
+}
+
+// удалить роль
+func removeVerifiedRole(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	role := i.ApplicationCommandData().Options[0].RoleValue(s, "")
+	database.RemoveVerifiedRole(i.GuildID, role.ID)
+	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Content: "Роль " + role.Mention() + " удалена",
+			Flags:   1 << 6,
+		},
+	})
+}
+
+// установить канал с логами
+func setWelcomeChannel(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	channel := i.ApplicationCommandData().Options[0].ChannelValue(s)
+	database.SetWelcomeChannel(i.GuildID, channel.ID)
+	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Content: "Канал " + channel.Mention() + " установлен",
+			Flags:   1 << 6,
+		},
+	})
+}
+
+// убрать канал с логами
+func removeWelcomeChannel(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	channel := i.ApplicationCommandData().Options[0].ChannelValue(s)
+	database.RemoveWelcomeChannel(i.GuildID, channel.ID)
+	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Content: "Канал " + channel.Mention() + " удален",
+			Flags:   1 << 6,
 		},
 	})
 }
